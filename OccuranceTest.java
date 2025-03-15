68463a298c2aff9240291efcb1eaffdd137cb938
richik96 <drichik@gmail.com>
2025-03-15 12:16:59 +0530

    full stream way

--- OccuranceTest.java
+++ OccuranceTest.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -6,11 +7,7 @@ public class OccuranceTest {
     public static void main(String[] args) {
 
         int[] arr = {1,4,2,7,1,8,2,4,1};
-
         Map<Integer, Integer> res = new HashMap<>();
-
-        int n = arr.length;
-
         for (int j : arr) {
             if (res.containsKey(j)) {
                 res.put(j, res.get(j) + 1);
@@ -18,7 +15,6 @@ public class OccuranceTest {
                 res.put(j, 1);
             }
         }
-
         for(Map.Entry<Integer, Integer> i : res.entrySet()) {
             System.out.println(i.getKey()+ " : "+i.getValue());
         }
--- WordLetterCount.java
+++ WordLetterCount.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -8,19 +9,26 @@ public class WordLetterCount {
 
 
         String res = "Richik Dutta Kolkata";
-
-        String[] resList = res.split(" ");
-
-        for(String s : resList) {
-            System.out.println(s);
-            Map<Character, Long> charCount = s.chars()
-                    .mapToObj(c -> (char) c)
-                    .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
-            //System.out.println(charCount);
-
-            for(Map.Entry<Character, Long> r : charCount.entrySet()) {
-                System.out.println(r.getKey() + "--> " + r.getValue());
-            }
-        }
+        Arrays.stream(res.split(" "))
+                .forEach(word -> {
+                    Map<Character, Long> result = word.chars()
+                            .mapToObj(c -> (char) c)
+                            .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+                    System.out.println("Word -> " + word);
+                    result.forEach((a, b) ->
+                            System.out.println(a + " ---> " + b));
+                });
     }
+//        for(String s : resList) {
+//            System.out.println(s);
+//            Map<Character, Long> charCount = s.chars()
+//                    .mapToObj(c -> (char) c)
+//                    .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+//            //System.out.println(charCount);
+//
+//            for(Map.Entry<Character, Long> r : charCount.entrySet()) {
+//                System.out.println(r.getKey() + "--> " + r.getValue());
+//            }
+//        }
+
 }
