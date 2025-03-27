47a374978412d5c68c9077fdddad6e32d968fc9a
richik96 <drichik@gmail.com>
2025-03-27 19:17:26 +0530

    27th march v3

--- SDE_Practice/LongestRepeatedChar.java
+++ SDE_Practice/LongestRepeatedChar.java
@@ -7,6 +7,20 @@ public class LongestRepeatedChar {
         String[] str = {"floor", "flood", "flower"};
 
         //find the longest repeated prefix characters in the array of strings
+        if(str == null || str.length == 0) {
+            System.out.println("");
+        }
+
+        String prefix = str[0];
+        for(int i=1; i<str.length; i++) {
+            while(str[i].indexOf(prefix) !=0) {
+                prefix = prefix.substring(0, prefix.length()-1);
+                if (prefix.isEmpty()) {
+                    System.out.println("");
+                }
+            }
+        }
+        System.out.println("Longest prefix :\n" +prefix);
 
     }
 }
